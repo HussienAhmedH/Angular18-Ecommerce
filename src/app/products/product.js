656cce143c30695a67a770c79977aa79310eb52f
Your Name <you@example.com>
2024-10-23 23:53:28 +0300

    products

---
+++ src/app/products/product.js
@@ -0,0 +1,8 @@
+function viewModal(){
+    document.getElementById("mySidenav").style.width = "75%";
+}
+
+function closeNavh() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mainh").style.marginLeft = "0";
+}
